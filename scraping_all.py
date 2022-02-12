4bd04bccab8868c391d226818d872d2eefa2897c
Dylan White <dylanwhite@cs.uchicago.edu>
2022-02-12 16:04:14 -0600

    yes

---
+++ scraping_all.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def get_info(soup):
+    '''
+
+    '''
+    phone = soup.find_all('p', class_="css-1h7ysrc")
+    phoneret = phone.tags[-2].text
+    address = soup.find_all('p', class_="css-1ccncw")
+    addressret = address.tags[0].text
+    web = soup.find_all('a', class_="css-10y60kr")
+    webret = web[2].get('href')
+    # restaurant name
+    name = soup.find_all('h1', class_="css-1x9iesk")[0].text
+    tags = soup.find_all('span', class_="css-1yy09vp")
+    num_review_str = tags[0].text
+    # number of reviews
+    num_review = re.search('^[0-9]+', num_review_str)[0]
+    # hours
+    time = tags[-1].text
+    rest_tags = set()
+    for i in range(1, len(tags)-1):
+        rest_tags.add(tags[i].find_all('a', class_= "css-1422juy")[0].text)
+    # rating
+    rating_tag = soup.find_all('div', class_="css-i-stars__09f24__foihJ i-stars--large-4__09f24__jGrzl border-color--default__09f24__NPAKY overflow--hidden__09f24___ayzG")
+    rating_str = re.findall('"([\w\s]+) star rating', str(soup))[0]
+    # price
+    price = -1
+    price_tags = soup.find_all('span', class_="css-oyv5ea")
+    for price_tag in price_tags:
+        if '$' in price_tag.text:
+            price = len(price_tag.text)
+    
