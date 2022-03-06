f600a864150040ff2f54d5409917be60dbba2039
Matthew Chen <mxchen@cs.uchicago.edu>
2022-03-06 17:26:59 -0600

    standard rec query working

--- recommendation.py
+++ recommendation.py
@@ -17,17 +17,19 @@ DATABASE_FILENAME = os.path.join(DATA_DIR, 'course-info.db')
 #   star rating (0-5), price (1-3) upper and lower bounds, boolean for standard or new
 # uery = 'SELECT rest_info.* FROM ' + ' WHERE ' + 'ORDER BY rest_info.bayes DESC LIMIT 10'
 
-def gen_query(db_fname, words = None, time_start = None, time_end = None, city = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
+def recommend(db_fname, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
+    query = gen_query()
+
+def gen_query(db_fname, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
     '''
     Inputs:
         db_fname (str): string indicating db filename
         words (str)
-        time_start (int)
-        time_end (int)
-        city (str)
+        time_start (str)
+        time_end (str)
         zipcode (str)
-        rating (float)
-        price (int)
+        rating (str)
+        price (str)
         try_new (bool)
     '''
     query = 'SELECT * FROM rest_info'
@@ -51,31 +53,39 @@ def gen_query(db_fname, words = None, time_start = None, time_end = None, city =
         print(word_args)
         if not word_checks:
             word_args = ''
-        else:
-            words_used = True
+        # else:
+        #     words_used = True
+    
+    non_word_param = False
 
     if time_start:
         where_args.append('time_start >= ' + time_start)
+        non_word_param = True
 
     if time_end:
         where_args.append('time_end <= ' + time_end)
-
-    if city:
-        where_args.append('city == ' + city)
+        non_word_param = True
 
     if zipcode:
         where_args.append('zipcode == ' + zipcode)
+        non_word_param = True
 
     if rating:
         where_args.append('rating >= ' + rating)
+        non_word_param = True
 
     if price_low:
         where_args.append('price <= ' + price_low)
         where_args.append('price != ' + '-1')
+        non_word_param = True
 
     if price_high:
         where_args.append('price <= ' + price_high)
         where_args.append('price != ' + '-1')
+        non_word_param = True
+
+    if words and non_word_param:
+        word_args += ' AND '
     
     # Query
 
@@ -89,10 +99,10 @@ def gen_query(db_fname, words = None, time_start = None, time_end = None, city =
 
     print(df[['rest_name', 'bayes', 'word']])
 
-    print(query)
+    #print(query)
     #need to detecet when u have words and other input, if thats the case add the AND to end of word_args, otherwise dont
 
-    # FIND LENGTH OF WORDS TABLE, get average tags per word (maybe add 1), MULTUPLY THIS BY NUMBER OF TAGS THAT THEY INPUT
+    # FIND LENGTH OF WORDS TABLE (15360), get average tags per word (15360 / 3183) (maybe add 1), MULTUPLY THIS BY NUMBER OF TAGS THAT THEY INPUT
 
     # take df, summarize by rest_name (get all the tags into like a list or something in one column, maybe just add strings),
     # write function to cmopare user input words to the tags, produce column for number of overlaps
