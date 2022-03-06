5d2b4ac0acff3d7184de59dd6ba632c98fac7690
Tara Tadjbakhsh <tara@Taras-MacBook-Pro.local>
2022-03-06 17:56:05 -0600

    interface

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ def find_user_info():
     print("What is you user ID?")
     userID = input()
     #userinfo will be the dataframe with all the information
-    matching_info = userinfo.loc[userinfo['UserId'] == userID]
+    #get matching_info from function
     if matching_info == None:
         print("We do not have a matching userID. If you are a new user input yes otherwise input no to try another userID")
         new_user = input()
@@ -10,6 +10,7 @@ def find_user_info():
             find_user_info()
         elif new_user == 'yes':
             print("As a new user, you have to input some of your eating preferences before proceeding")
+            #call function to get a new entry in
             new_entry()
         else:
             print("Incorrect input, please try again")
@@ -18,22 +19,29 @@ def find_user_info():
         print("Input entry if you want to submit a new eating entry or input reccomendation if you want a reccomendation")
         looking_for = input()
         if looking_for == 'entry':
+            #call function to get a new entry in
             new_entry()
         elif looking_for == 'reccomendation':
+            try_new = get_try_new()
             print("You will be prompted for inputs. Please follow the format indicated and if you don't have a preference press enter")
+            if try_new == False:
+                tags = get_tags()
             questions_for_reccomendation()
+            #call function to get a reccomendation
         else:
             print("Incorrect input, please try again")
             find_user_info()
 
 def new_entry():
     print("Information asked about what they ate")
-    #get info on what they ate and add it to the dataframe of user data
+    #returns needed information on what was eaten
 
 def questions_for_reccomendation():
-    tags = get_tags()
     open_time = get_open_time()
     close_time = get_close_time()
+    zipcode = get_zipcode()
+    rating = get_rating()
+    price = get_price()
 
 def get_tags():
     print("What kind of restaurant are you looking for? (Input tags with a space in between or input suggest to get a list of suggested tags)")
@@ -87,3 +95,36 @@ def get_zipcode():
     else:
         print("Incorrect input, please try again")
         get_zipcode()
+
+def get_rating():
+    print("What is the minimum rating you are looking for? (rated out of 5 and a decimal can be inputed)")
+    rating = input()
+    if rating.isnumeric() and float(rating) >= 0 and float(rating) <= 5:
+        return rating
+    elif rating == '':
+        return None
+    else:
+        print("Incorrect input, please try again")
+        get_rating()
+
+def get_price():
+    print("What price range are you looking for? (input an integer between 1 and 4 where 1 represents under $10, 2 represents $11-$30, 3 represents $31-$60, and 4 represents above $61)")
+    price = input()
+    if price.isnumeric() and (int(price) == 1 or int(price) == 2 or int(price) == 3 or int(price) == 4):
+        return price
+    elif price == '':
+        return None
+    else:
+        print("Incorrect input, please try again")
+        get_price()
+
+def get_try_new():
+    print("Input 1 if you would like to get a reccomendation based on specific inputs or 2 if you would like a randomly selected restaurant based on things you haven't tried yet")
+    try_new = input()
+    if try_new == 1:
+        return True
+    elif try_new == 2:
+        return False
+    else:
+        print("Incorrect input, please try again")
+        get_try_new()
