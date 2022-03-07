f18066e82c14643ec7113b850ed355343074c69a
Matthew Chen <mxchen@cs.uchicago.edu>
2022-03-06 19:19:04 -0600

    continued working on recommendation function

--- UI/recommendation.py
+++ UI/recommendation.py
@@ -1,6 +1,8 @@
 # Provides functions to recommend
 # MAYBE DO PANDAS TO SQL INSEAD OF CSV TO SQL
 
+# TO DO: finish get_limit, fix bayes rounding by converting to int, account for try_new (need to wait for data structure), get group_by to work
+
 # Import libraries
 import os
 import sqlite3
@@ -9,7 +11,7 @@ import pandas as pd
 
 # Path to database
 DATA_DIR = os.path.dirname(__file__)
-DATABASE_FILENAME = os.path.join(DATA_DIR, 'course-info.db')
+DATABASE_FILENAME = os.path.join(DATA_DIR, 'rest_db.db')
 
 
 # Take in inputs
@@ -18,12 +20,12 @@ DATABASE_FILENAME = os.path.join(DATA_DIR, 'course-info.db')
 # uery = 'SELECT rest_info.* FROM ' + ' WHERE ' + 'ORDER BY rest_info.bayes DESC LIMIT 10'
 
 def recommend(user_id, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
-    query = gen_query(user_id, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False)
-
+    query = gen_query(user_id, words, time_start, time_end, zipcode, rating, price_low, price_high, try_new)
+    return process_query(query)
     
 
 
-def gen_query(db_fname, user_id, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
+def gen_query(user_id, words = None, time_start = None, time_end = None, zipcode = None, rating = None, price_low = None, price_high = None, try_new = False):
     '''
     Inputs:
         db_fname (str): string indicating db filename
@@ -48,16 +50,13 @@ def gen_query(db_fname, user_id, words = None, time_start = None, time_end = Non
     if words:
         word_checks = []
         for word in words.split():
-            print(word)
             if len(word) > 2:
                 word_checks.append('word LIKE \'%' + word + '%\'')
-                print('appended word')
         word_args = '(' + ' OR '.join(word_checks) + ')'
-        print(word_args)
         if not word_checks:
             word_args = ''
-        # else:
-        #     words_used = True
+        else:
+            words_used = True
     
     non_word_param = False
 
@@ -87,37 +86,61 @@ def gen_query(db_fname, user_id, words = None, time_start = None, time_end = Non
         where_args.append('price != ' + '-1')
         non_word_param = True
 
-    if words and non_word_param:
-        word_args += ' AND '
+    #soajdsoijdsioa
+    if words or non_word_param:
+        query += ' WHERE '
+        if words and non_word_param:
+            word_args += ' AND '
+
     
     # Query
 
     # limit is 10 if no words provided, if words provided see comment below
-    query += ' WHERE ' + word_args + ' AND '.join(where_args) + ' ORDER BY bayes DESC LIMIT 100' + ';'
+    limit = get_limit()
+    query += word_args + ' AND '.join(where_args) + ' ORDER BY bayes DESC LIMIT ' + limit + ';'
 
     return query
 
+
+def get_limit():
+    '''
+    '''
+    # FIND LENGTH OF WORDS TABLE (15360), get average tags per word (15360 / 3183) (maybe add 1), MULTUPLY THIS BY NUMBER OF TAGS THAT THEY INPUT
+    db = sqlite3.connect(DATABASE_FILENAME)
+    num_tags = 0
+    num_rest = 0
+    return str(200)
 
 
 def process_query(query):
     '''
     '''
-    db = sqlite3.connect(db_fname)
-
+    print(query)
+    db = sqlite3.connect(DATABASE_FILENAME)
     df = pd.read_sql_query(query, db)
     db.close()
 
-    print(df[['rest_name', 'bayes', 'word']])
+    df = df.loc[:,~df.columns.duplicated()]
+    print(df.columns)
 
-    #print(query)
-    #need to detecet when u have words and other input, if thats the case add the AND to end of word_args, otherwise dont
+    print(df[['rest_name', 'word']])
 
-    # FIND LENGTH OF WORDS TABLE (15360), get average tags per word (15360 / 3183) (maybe add 1), MULTUPLY THIS BY NUMBER OF TAGS THAT THEY INPUT
 
     # take df, summarize by rest_name (get all the tags into like a list or something in one column, maybe just add strings),
     # write function to cmopare user input words to the tags, produce column for number of overlaps
+    df.groupby('id').size() # right now the only words found are the words that are searched for, somehow need to get all the words
+    print(df)
 
-    # sort  bayes to nearest int or 0.5 int or something, then sort using two conditions 1. bayes, and 2. tag overlaps
-    # split function up
+    print(df.dtypes)
+
+    # sort  bayes to nearest int or 0.5 int
+    df['rounded_bayes'] = df['bayes'].mul(2).round().div(2)
+
+    # sort using two conditions 1. bayes, and 2. tag overlaps
+    df.sort_values(['rounded_bayes', 'tag_overlaps'], ascending=False, inplace=True)
+
+    print(df.head(10))
+
+    return df.head(10)
 
 
