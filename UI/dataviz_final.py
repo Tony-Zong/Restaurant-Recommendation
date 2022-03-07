194af93bc5855b2b7e8e9b05ee33524f1666924e
Tony Zong <htzong@cs.uchicago.edu>
2022-03-07 16:15:31 -0600

    UI functions

--- UI/dataviz_final.py
+++ UI/dataviz_final.py
@@ -7,6 +7,37 @@ import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 from datetime import date , timedelta
 
+DF_FILENAME = 'user_info.pickle'
+
+
+def check_user_exists(username):
+    '''
+    Check whether the inputted username already exists.
+    '''
+
+    f = open(DF_FILENAME, 'rb')
+    user_info = p.load(f)
+    return username in df['user'].unique()
+
+
+def add_row(user, date, rest, cuisine, user_rating, cost):
+    '''
+    Update the user_info data frame when the user inputs information about a meal.
+    '''
+
+    f = open(DF_FILENAME, 'rb')
+    user_info = p.load(f)
+
+    to_append = {'user': user, 'date': date, 'rest': rest, 'cuisine': cuisine,
+                 'user_rating': user_rating, 'cost': cost}
+
+    user_info = user_info.append(to_append, ignore_index = True)
+
+    f2 = open(DF_FILENAME, 'wb')
+    pickle.dump(user_info, f2, pickle.HIGHEST_PROTOCOL)
+    f2.close()
+
+    
 
 # start date and end date are None by default. if this is the case return dataviz
 # for all dates in date range
